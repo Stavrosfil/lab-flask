9028d3728bd50fc180d58889f3f7771333a246f0
Stavros Filosidis <stavrosfil@gmail.com>
2020-03-25 07:45:32 +0200

    Drop sqlite support - Drop blueprints for authentication in favor of simplehttp - Change requirements

--- laboratorium/__init__.py
+++ laboratorium/__init__.py
@@ -1,25 +1,19 @@
 import os
-from . import db
 from flask import Flask, g, request
 from flask_redis import FlaskRedis
 from flask_restful import Resource, Api
-from laboratorium import routes
 from flask_influxdb import InfluxDB
-
+from flask_pymongo import PyMongo
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from laboratorium import routes
+
 r = FlaskRedis()
 influx = InfluxDB()
+mongo = PyMongo()
 auth = HTTPBasicAuth()
 users = {}
-
-
-@auth.verify_password
-def verify_password(username, password):
-    if username in users:
-        return check_password_hash(users.get(username), password)
-    return False
 
 
 def create_app(test_config=None):
@@ -30,8 +24,6 @@ def create_app(test_config=None):
     # Load app config file
     app.config.from_pyfile("config.py", silent=False)
 
-    users.update({app.config["ADMIN"]: generate_password_hash(app.config["ADMIN_PW"])})
-
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
@@ -39,13 +31,25 @@ def create_app(test_config=None):
         pass
 
     with app.app_context():
+
+        users.update({
+            app.config["ADMIN"]:
+            generate_password_hash(app.config["ADMIN_PW"]),
+        })
+
         # Initialize global objects
         r.init_app(app)
         influx.init_app(app)
-
         api = Api(app)
+        mongo.init_app(app, app.config["MONGO_URI"])
+
         routes.init_routes(api)
 
-        db.init_app()
-
         return app
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users:
+        return check_password_hash(users.get(username), password)
+    return False
