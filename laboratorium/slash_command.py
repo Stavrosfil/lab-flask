e2763fb823b709979b6a75f45778d175484f2059
Stavros Filosidis <stavrosfil@gmail.com>
2020-08-06 18:35:53 +0000

    Use the same response format within all functions -Add simple check for command existance

--- laboratorium/slash_command.py
+++ laboratorium/slash_command.py
@@ -26,8 +26,19 @@ class SlashLab(Resource):
         data = message[(len(command) if len(command) == 0 else len(command) + 1) :]
 
         func = cases.get(command)
+        print(func, command)
+        if func is None:
+            return jsonify(response_format("This command does not exist.", "!"))
         payload = func(data)
         return jsonify(payload)
+
+
+def response_format(text, image_format="HI"):
+    return {
+        "text": text,
+        "username": "Laboratorium",
+        "icon_url": "https://eu.ui-avatars.com/api/?name={}".format(image_format),
+    }
 
 
 def status(data):
@@ -43,26 +54,15 @@ def status(data):
             # user = mongo_users.find_one({"_id": user})
             user = User.User({"_id": user})
             message += f"{user.first_name} {user.last_name}\n"
-        payload = {
-            "text": message,
-            "username": "Lab Authenticator",
-            "icon_url": "https://eu.ui-avatars.com/api/?name={}".format(len(users)),
-        }
-        return payload
+        return response_format(message, len(users))
 
 
 def stats(data):
     lab = mongo_labs.find_one({"_id": "lab1"})
-    payload = {
-        "text": "The lab statistics for today! {}, {}".format(
-            lab["user_count"], lab["_id"]
-        ),
-        "username": "Lab Stats",
-        "icon_url": "https://eu.ui-avatars.com/api/?name={}".format(
-            request.form["user_name"]
-        ),
-    }
-    return payload
+    return response_format(
+        "The lab statistics for today! {}, {}".format(lab["user_count"], lab["_id"]),
+        request.form["user_name"],
+    )
 
 
 def checkout(data):
@@ -72,40 +72,18 @@ def checkout(data):
 
     if user.lab_uuid != "0":
         user.checkout()
-        payload = {
-            "text": "The user {} was successfully checked out!".format(
-                user.mm_username
-            ),
-            "username": "Lab Authenticator",
-            "icon_url": "https://eu.ui-avatars.com/api/?name={}".format(
-                user.first_name
-            ),
-        }
+        text = "The user {} was successfully checked out!".format(user.mm_username)
+        payload = response_format(text, user.first_name)
     else:
-        payload = {
-            "text": "You are not checked in any lab!",
-            "username": "Lab Authenticator",
-            "icon_url": "https://eu.ui-avatars.com/api/?name={}".format(
-                user.first_name
-            ),
-        }
+        payload = response_format("You are not checked in any lab!", user.first_name)
     return payload
-
-
-def count(data):
-    pass
 
 
 def mock(data):
     ans = []
     for c in data:
         ans.append(c.upper() if random.randint(0, 1) == 1 else c.lower())
-    payload = {
-        "text": "".join(ans),
-        "username": "lAB AutHeNTIcAtOr",
-        "icon_url": "https://eu.ui-avatars.com/api/?name={}".format("M"),
-    }
-    return payload
+    return response_format("".join(ans), "M")
 
 
 def add_tag(data):
@@ -126,14 +104,9 @@ def add_tag(data):
     else:
         text = "Wrong request format"
 
-    payload = {
-        "text": text,
-        "username": "Lab Authenticator",
-        "icon_url": "https://eu.ui-avatars.com/api/?name={}".format(text),
-    }
-    return payload
+    return response_format(text, text)
 
 
 def clear_queue(data):
     queue.clear()
-    return "clear"
+    return response_format("Queue was cleared successfully.")
